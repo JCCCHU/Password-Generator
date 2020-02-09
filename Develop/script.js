3bc88c6426d6a76c00e50ac6d5f8eeee8f05203a
Joshua Chu <jashya@gmail.com>
2020-02-08 23:57:14 -0500

    Add comments, fix README

--- Develop/script.js
+++ Develop/script.js
@@ -19,19 +19,26 @@ function generatePassword() {
                        [false, "ABCDEFGHIJKLMNOPQRSTUVWXYZ"],
                        [false, "1234567890"],
                        [false, "!@#$%^&*()-=_+[]{}|;':,.<>/?"]];
+    
+    //String with all valid characters
     var validChars = "";
-                       
+    //Variable for password-in-progress                   
     var passwordBuild = "";
+
+    //Asks the user for a password length until they give a valid one
     do {
       pwLength = prompt("Enter a length for your password (8-128 chars):");
       console.log("1: " + pwLength);
     } while ((pwLength > 128) || (pwLength < 8));
     do {
+      //Checks whether the user has entered a valid character type
       var valid = false;
+      //Prompts the user to select character types by entering a string with characters corresponding to each type.
       var pwInput = prompt("Enter the character types you want in your password.\n'a' for lowercase letters\n'b' for UPPERCASE LETTERS\n'c' for numb3r5\n'd' for $pec!al characters");
       pwInput.toLowerCase();
       console.log("2: " + pwInput);
       for (var i = 0; i < pwInput.length; i++) {
+        //Checks each character input for validity, then modifies the pwCharTypes array to mark it as active
         switch (pwInput[i]) {
           case "a":
             pwCharTypes[0][0] = true;
@@ -51,11 +58,14 @@ function generatePassword() {
             break;
         }
       }
+
+      //Checks if the user has entered at least 1 valid character type
       if (!valid) {
         alert("Please only enter the characters a, b, c, or d.");
       }
       console.log("3: " + valid);
     } while (!valid);
+    //Adds selected character types to the list of valid characters to build the password from
     for (i = 0; i < pwCharTypes.length; i++) {
       if (pwCharTypes[i][0]) {
         validChars += pwCharTypes[i][1];
@@ -63,6 +73,7 @@ function generatePassword() {
     }
     console.log("4: " + validChars);
     console.log("4.5: " + pwLength);
+    //Generates a random valid character, then adds it to the password until the appropriate length has been reached.
     for (i = 0; i < pwLength; i++) {
       genChar = validChars[Math.floor(Math.random() * validChars.length)];
       passwordBuild += genChar;
